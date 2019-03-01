a2ed148ef0e6e39d4ceda908487aa3efdb0e89cd
Vajniy <sanya92t@gmail.com>
2019-03-01 20:35:20 +0200

    commit

--- worktime/worktime.py
+++ worktime/worktime.py
@@ -12,17 +12,11 @@ def timer():
     globl = int(log.read())
     log.close()
     print('* Global working time at this moment is: ' + str(datetime.timedelta(seconds = globl)))
-    if input('Press any key for end this session '):
-        timeofnews = int(-float(nwsess - time.time()))
-        newglobl = globl + timeofnews
-        print('>> Time of this session is: ' + str(datetime.timedelta(seconds = timeofnews)))
-        print('>> Global: ' + str(datetime.timedelta(seconds = newglobl)))
-        open('log.txt', 'w').write(str(newglobl))
-    else:
-        timeofnews = int(-float(nwsess - time.time()))
-        newglobl = globl + timeofnews
-        print('>> Time of this session is: ' + str(datetime.timedelta(seconds = timeofnews)))
-        print('>> Global: ' + str(datetime.timedelta(seconds = newglobl)))
-        open('log.txt', 'w').write(str(newglobl))
+    input('Press any key for end this session ')
+    timeofnews = int(-float(nwsess - time.time()))
+    newglobl = globl + timeofnews
+    print('>> Time of this session is: ' + str(datetime.timedelta(seconds = timeofnews)))
+    print('>> Global: ' + str(datetime.timedelta(seconds = newglobl)))
+    open('log.txt', 'w').write(str(newglobl))
 
 timer()
