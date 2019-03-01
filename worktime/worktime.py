b971a4bcb58334269561a0752ba203f90921aa96
Vajniy <sanya92t@gmail.com>
2019-03-01 16:33:47 +0200

    Adding WorkTime prog

---
+++ worktime/worktime.py
@@ -0,0 +1,28 @@
+import time, os, sys, datetime
+
+#format
+#reworking
+
+'''Programm for counting your time work'''
+
+def timer():
+    print('* Session is started')
+    nwsess = time.time()
+    log = open('log.txt', 'r')
+    globl = int(log.read())
+    log.close()
+    print('* Global working time at this moment is: ' + str(datetime.timedelta(seconds = globl)))
+    if input('Press any key for end this session '):
+        timeofnews = int(-float(nwsess - time.time()))
+        newglobl = globl + timeofnews
+        print('>> Time of this session is: ' + str(datetime.timedelta(seconds = timeofnews)))
+        print('>> Global: ' + str(datetime.timedelta(seconds = newglobl)))
+        open('log.txt', 'w').write(str(newglobl))
+    else:
+        timeofnews = int(-float(nwsess - time.time()))
+        newglobl = globl + timeofnews
+        print('>> Time of this session is: ' + str(datetime.timedelta(seconds = timeofnews)))
+        print('>> Global: ' + str(datetime.timedelta(seconds = newglobl)))
+        open('log.txt', 'w').write(str(newglobl))
+
+timer()
