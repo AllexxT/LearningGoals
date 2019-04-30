caa2750fd1477c16f83813397776fde78975dc8d
Vajniy <sanya92t@gmail.com>
2019-04-30 23:16:58 +0300

    first working version of convertator numbers

--- RomaToArabic/ArabicToRoma.py
+++ RomaToArabic/ArabicToRoma.py
@@ -43,7 +43,8 @@ class Stack:
     def __repr__(self):
         return str(self.items)
 
-
+# Конвертирует римские цифры в арабские
+# Используется стек(код выше)
 def toArabic(number):
     stack = Stack()
     roma = {'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000}
@@ -61,8 +62,10 @@ def toArabic(number):
     result = sum(stack)
     return result
 
-def toRoma(number):
-    roma = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+
+# Разделяет число на слагаемые
+# Прим.: 246 == [200, 40, 6]
+def splitToAdditions(number):
     adt = []
     num = str(number)
     for x in range(len(str(number))):
@@ -72,10 +75,89 @@ def toRoma(number):
         tm = ' '.join(num).split()
         tm.pop(0)
         num = ''.join(tm)
-    print(adt)
+    return adt
+
+# Обрезает число возвращая первую цифру слева, если она <= 3
+# Прим.: 240 == 2, 350 == 3, 450 == None
+def tailCut(numb):
+    tmp = int(str(numb)[:1])
+    if tmp == 4:
+        return 4
+    elif tmp > 3:
+        return None
+    return tmp
+
+# Выбирает римскую букву для арабской цифры
+def chooseLetter(number=200):
+    top = [6, 7, 8, 60, 70, 80, 600, 700, 800]
+    roma = 'Nan'
+    tC = tailCut(number)
+    if number in list(range(1,4)):
+        roma = 'I'
+    elif number in [10,20,30]:
+        roma = 'X'
+    elif number in [100,200,300]:
+        roma = 'C'
+    elif number in [1000,2000,3000]:
+        roma = 'M'
+    elif number == 4:
+        return 'IV'
+    elif number == 40:
+        return 'XL'
+    elif number == 9:
+        return 'IX'
+    elif number == 90:
+        return 'XC'
+    elif number == 900:
+        return 'CM'
+    elif number == 400:
+        return 'CD'
+    elif number in [5]:
+        return  'V'
+    elif number in [50]:
+        return  'L'
+    elif number in [500]:
+        return  'D'
+    elif number in top:
+        for x in top:
+            if number % 5 != 0:
+                incr = chooseLetter(number % 5)
+                tm = ''
+                for x in incr:
+                    tm += x
+                return 'V' + tm
+            elif number % 50 != 0:
+                incr = chooseLetter(number % 50)
+                tm = ''
+                for x in incr:
+                    tm += x
+                return 'L' + tm
+            elif number % 500 != 0:
+                incr = chooseLetter(number % 500)
+                tm = ''
+                for x in incr:
+                    tm += x
+                return 'D' + tm
+    return [roma for x in range(tC)]
 
 
-#one = int(str(x)[:1]+'0'*(len(str(x))-1))
+def toRoma(num):
+    roma = {1:'I', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+    split = splitToAdditions(num)
+    res = ''
+    for x in split:
+        for y in chooseLetter(x):
+            res += y
+    return res
+
+
+def convert(num):
+    if type(num) == str:
+        return toArabic(num)
+    else:
+        return toRoma(num)
+
+
 
 
 
@@ -84,7 +166,15 @@ if __name__ == '__main__':
     def tests():
         rm = ['X', 'L', 'C', 'D', 'M']
         arab = [10, 50, 100, 500, 1000]
+        print('Testing roma to arabic numbers convertation')
         for x in range(len(rm)):
-            assert toArabic(rm[x]) == arab[x], 'Test №%d is FAILED!' % x
+            assert convert(rm[x]) == arab[x], 'Test №%d is FAILED!' % x
+            print('{} -> {}'.format(rm[x], arab[x]))
             print('test %s is passed' % str(x+1))
+        print('Testing arabic to roma numbers convertation')
+        for x in range(len(rm)):
+            assert convert(arab[x]) == rm[x], 'Test №%d is FAILED!' % x
+            print('{} -> {}'.format(arab[x], rm[x]))
+            print('test %s is passed' % str(x+1))
+        
         print('All tests is passed!')
